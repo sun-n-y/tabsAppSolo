cbed46d762803898bfb5150a0521d16e1b7227db
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-30 10:31:44 -0400

    add fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchJobs = async () => {
+    setIsLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    setJobs(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return <h1>loading...</h1>;
+  }
+
+  console.log(jobs);
+
   return <h1>h</h1>;
 }
 
